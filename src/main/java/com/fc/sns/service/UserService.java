f01ac915bf3c6988fdcc222769e1db9acdd1c91c
webdevyoo <yjk7454@naver.com>
2024-05-06 14:49:53 +0900

    Ch02-05: `UserSerivce` - 회원 가입/로그인 선행 테스트용 임시 기능구현

---
+++ src/main/java/com/fc/sns/service/UserService.java
@@ -0,0 +1,43 @@
+package com.fc.sns.service;
+
+import com.fc.sns.exception.SnsApplicationException;
+import com.fc.sns.model.User;
+import com.fc.sns.model.entity.UserEntity;
+import com.fc.sns.repository.UserEntityRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserEntityRepository userEntityRepository;
+
+    // TODO: implement
+    public User join(String userName, String password) {
+        // 회원 가입 하려는 userName으로 회원 가입된 user가 있는지
+        Optional<UserEntity> userEntity = userEntityRepository.findByUserName(userName);
+
+        // 회원 가입 진행 = user를 등록
+        userEntityRepository.save(new UserEntity());
+
+        return new User();
+    }
+
+    // TODO: implement
+    public String login(String userName, String password) {
+        // 회원가입 여부 체크
+        UserEntity userEntity = userEntityRepository.findByUserName(userName).orElseThrow(() -> new SnsApplicationException());
+
+        // 비밀번호 체크
+        if(!userEntity.getPassword().equals(password)) {
+            throw new SnsApplicationException();
+        }
+        
+        // 토큰 생성
+
+        return "";
+    }
+}
