ada5527c976bb339c1df0ddefadcd8d601a3e978
webdevyoo <yjk7454@naver.com>
2024-05-08 09:55:41 +0900

    Ch02-06: `UserDto` - 변경된 회원 엔티티 상세 필드에 맞춰 필드 추가, Entity DTO 변환 팩토리메소드 구현

--- src/main/java/com/fc/sns/model/User.java
+++ src/main/java/com/fc/sns/model/User.java
@@ -1,13 +1,37 @@
 package com.fc.sns.model;
 
+import com.fc.sns.model.entity.UserEntity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.sql.Timestamp;
+
 /**
  * 실제 회원 조회후 반환되는 Entity를 대신하여
  * 회원 정보를 제공해줄 수 있도록 도와주는 User DTO용도의 객체
  * 회원정보 응답 spec이 변경 등, 의존성 분리 혹은
  * OSIV 로 인한 커넥션 이슈 성능저하 등을 위해 대게는 DTO로 따로 관리한다.
  */
-// TODO: implement
+@AllArgsConstructor
+@Getter
 public class User {
+    private Integer id;
     private String userName;
     private String password;
+    private UserRole userRole;
+    private Timestamp registeredAt;
+    private Timestamp updatedAt;
+    private Timestamp deletedAt;
+
+    public static User fromEntity(UserEntity entity) {
+        return new User(
+                entity.getId(),
+                entity.getUserName(),
+                entity.getPassword(),
+                entity.getRole(),
+                entity.getRegisteredAt(),
+                entity.getUpdatedAt(),
+                entity.getDeletedAt()
+        );
+    }
 }
