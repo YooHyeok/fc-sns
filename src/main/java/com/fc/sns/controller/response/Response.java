7ef4b32cfbb173f383454f84aeaa3d7a4701c9e0
webdevyoo <yjk7454@naver.com>
2024-05-08 09:51:29 +0900

    Ch02-06: `Response` - 공통 응답 클래스 추가

---
+++ src/main/java/com/fc/sns/controller/response/Response.java
@@ -0,0 +1,24 @@
+package com.fc.sns.controller.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class Response<T> {
+    private String resultCode;
+    private T result;
+
+    /**
+     * result 가 null 이므로 Generic은 Void로 지정한다.
+     * @param errorCode
+     * @return
+     */
+    public static Response<Void> error(String errorCode) {
+        return new Response<>(errorCode, null);
+    }
+
+    public static <T> Response<T> success(T result) {
+        return new Response<>("SUCCESS", result);
+    }
+}
