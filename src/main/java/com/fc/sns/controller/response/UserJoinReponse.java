a03c5950d3b6dfc0a7e81fbb4bfecd808951968a
webdevyoo <yjk7454@naver.com>
2024-05-08 09:51:54 +0900

    Ch02-06: `UserJoinResponse` - 회원 가입 응답 클래스 추가

---
+++ src/main/java/com/fc/sns/controller/response/UserJoinReponse.java
@@ -0,0 +1,24 @@
+package com.fc.sns.controller.response;
+
+import com.fc.sns.model.User;
+import com.fc.sns.model.UserRole;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class UserJoinReponse {
+
+    private Integer id;
+    private String userName;
+    private UserRole role;
+
+    public static UserJoinReponse fromUser(User user) {
+        return new UserJoinReponse(
+            user.getId(),
+            user.getUserName(),
+            user.getUserRole()
+        );
+    }
+
+}
