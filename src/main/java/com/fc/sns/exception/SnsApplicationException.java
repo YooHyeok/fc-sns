524ad7f0f107f787471f806e800e4a72557ecdde
webdevyoo <yjk7454@naver.com>
2024-05-08 09:53:42 +0900

    Ch02-06: `SnsApplicationException` - 공통 Exception에 오류코드 관리 공통 클레스 적용

--- src/main/java/com/fc/sns/exception/SnsApplicationException.java
+++ src/main/java/com/fc/sns/exception/SnsApplicationException.java
@@ -1,5 +1,17 @@
 package com.fc.sns.exception;
 
-// TODO : implement
-public class SnsApplicationException extends RuntimeException{
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class SnsApplicationException extends RuntimeException {
+    private ErrorCode errorCode;
+    private String message;
+
+    @Override
+    public String getMessage() {
+        if (message == null) return errorCode.getMessage();
+        return String.format("%s. %s", errorCode.getMessage(), message);
+    }
 }
