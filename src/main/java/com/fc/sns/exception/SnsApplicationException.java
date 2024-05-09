cc753ea277ff7aebcc126e2ba5d289abd078d978
webdevyoo <yjk7454@naver.com>
2024-05-10 00:46:05 +0900

    Ch02-06: `SnsApplicationException` - 에러 메시지를 null로 처리할 수 있도록 하는 생성자 추가

--- src/main/java/com/fc/sns/exception/SnsApplicationException.java
+++ src/main/java/com/fc/sns/exception/SnsApplicationException.java
@@ -9,6 +9,16 @@ public class SnsApplicationException extends RuntimeException {
     private ErrorCode errorCode;
     private String message;
 
+    /**
+     * 에러 코드만 받고 나머지는 null로 처리할 경우
+     * ex) password 오류에서 password정보를 알릴 필요는 없..
+     * @param errorCode
+     */
+    public SnsApplicationException(ErrorCode errorCode) {
+        this.errorCode = errorCode;
+        this.message = null;
+    }
+
     @Override
     public String getMessage() {
         if (message == null) return errorCode.getMessage();
