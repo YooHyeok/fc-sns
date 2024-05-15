1b22f9174ed6632d97a5f85d54796de3c1169a19
webdevyoo <yjk7454@naver.com>
2024-05-15 11:44:22 +0900

    Ch.02-09: `PostControllerTest` - 게시글 수정API 테스트 구현(정상, 미인증, 권한불일치, 글없음)

--- src/test/java/com/fc/sns/controller/PostControllerTest.java
+++ src/test/java/com/fc/sns/controller/PostControllerTest.java
@@ -2,9 +2,14 @@ package com.fc.sns.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fc.sns.controller.request.PostCreateRequest;
+import com.fc.sns.controller.request.PostModifyRequest;
 import com.fc.sns.controller.request.UserJoinRequest;
+import com.fc.sns.exception.ErrorCode;
+import com.fc.sns.exception.SnsApplicationException;
 import com.fc.sns.service.PostService;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentMatchers;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -58,5 +63,79 @@ public class PostControllerTest {
                                 .content(objectMapper.writeValueAsBytes(new PostCreateRequest(title, body)))
                 ).andDo(MockMvcResultHandlers.print()) // 출력
                 .andExpect(MockMvcResultMatchers.status().isUnauthorized()); // 정상 동작
+    }
+
+    @Test
+    @WithMockUser
+    void 포스트수정() throws Exception{
+        String title = "title";
+        String body = "body";
+
+        //로그인 하지 않은 경우
+        mockMvc.perform(
+                        MockMvcRequestBuilders.put("/api/v1/posts/1")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                //TODO : add request body
+                                .content(objectMapper.writeValueAsBytes(new PostModifyRequest(title, body)))
+                ).andDo(MockMvcResultHandlers.print()) // 출력
+                .andExpect(MockMvcResultMatchers.status().isOk()); // 정상 동작
+    }
+
+    @Test
+    @WithAnonymousUser // 익명의 회원으로 요청을날림
+    void 포스트수정시_로그인하지않은경우() throws Exception{
+        String title = "title";
+        String body = "body";
+
+        //로그인 하지 않은 경우
+        mockMvc.perform(
+                        MockMvcRequestBuilders.put("/api/v1/posts/1")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                //TODO : add request body
+                                .content(objectMapper.writeValueAsBytes(new PostModifyRequest(title, body)))
+                ).andDo(MockMvcResultHandlers.print()) // 출력
+                .andExpect(MockMvcResultMatchers.status().isUnauthorized());
+    }
+
+    @Test
+    @WithMockUser
+    void 포스트수정시_본인이_작성한_글이_아니라면_에러발생() throws Exception{
+        String title = "title";
+        String body = "body";
+
+        // mocking postService의 modify가 호출될때 Error throw
+        Mockito.doThrow(new SnsApplicationException(ErrorCode.INVALID_PERMISSION))
+                .when(postService)
+                .modify(ArgumentMatchers.eq(title), ArgumentMatchers.eq(body), ArgumentMatchers.any(), ArgumentMatchers.eq(1));
+
+        //로그인 하지 않은 경우
+        mockMvc.perform(
+                        MockMvcRequestBuilders.put("/api/v1/posts/1")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                //TODO : add request body
+                                .content(objectMapper.writeValueAsBytes(new PostModifyRequest(title, body)))
+                ).andDo(MockMvcResultHandlers.print()) // 출력
+                .andExpect(MockMvcResultMatchers.status().isUnauthorized());
+    }
+
+    @Test
+    @WithMockUser
+    void 포스트수정시_수정하려는_글이_없는경우_에러발생() throws Exception{
+        String title = "title";
+        String body = "body";
+
+        // mocking
+        Mockito.doThrow(new SnsApplicationException(ErrorCode.POST_NOT_FOUND))
+                .when(postService)
+                .modify(ArgumentMatchers.eq(title), ArgumentMatchers.eq(body), ArgumentMatchers.any(), ArgumentMatchers.eq(1));
+
+        //로그인 하지 않은 경우
+        mockMvc.perform(
+                        MockMvcRequestBuilders.put("/api/v1/posts/1")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                //TODO : add request body
+                                .content(objectMapper.writeValueAsBytes(new PostModifyRequest(title, body)))
+                ).andDo(MockMvcResultHandlers.print()) // 출력
+                .andExpect(MockMvcResultMatchers.status().isNotFound());
     }
 }
