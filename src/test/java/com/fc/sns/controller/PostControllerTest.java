ee8f11ea2c4c469417f093354e1e09693a5e3148
webdevyoo <yjk7454@naver.com>
2024-05-18 15:19:52 +0900

    Ch.02-13: `PostControllerTest` - 피드목록 및 작성한 포스트 목록 조회 컨트롤러 API 테스트 구현

--- src/test/java/com/fc/sns/controller/PostControllerTest.java
+++ src/test/java/com/fc/sns/controller/PostControllerTest.java
@@ -16,6 +16,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.Page;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithAnonymousUser;
 import org.springframework.security.test.context.support.WithMockUser;
@@ -39,7 +40,7 @@ public class PostControllerTest {
 
     @Test
     @WithMockUser // 인증된 회원
-    void 포스트작성() throws Exception{
+    void 포스트작성() throws Exception {
         String title = "title";
         String body = "body";
         mockMvc.perform(
@@ -52,7 +53,7 @@ public class PostControllerTest {
 
     @Test
     @WithAnonymousUser // 익명의 회원으로 요청을날림
-    void 포스트작성시_로그인하지않은경우() throws Exception{
+    void 포스트작성시_로그인하지않은경우() throws Exception {
         String title = "title";
         String body = "body";
 
@@ -67,7 +68,7 @@ public class PostControllerTest {
 
     @Test
     @WithMockUser
-    void 포스트수정() throws Exception{
+    void 포스트수정() throws Exception {
         String title = "title";
         String body = "body";
 
@@ -75,7 +76,6 @@ public class PostControllerTest {
         Mockito.when(postService.modify(ArgumentMatchers.eq(title), ArgumentMatchers.eq(body), ArgumentMatchers.any(), ArgumentMatchers.any()))
                 .thenReturn(Post.fromEntity(PostEntityFixture.get("userName", 1, 1)));
 
-        //로그인 하지 않은 경우
         mockMvc.perform(
                         MockMvcRequestBuilders.put("/api/v1/posts/1")
                                 .contentType(MediaType.APPLICATION_JSON)
@@ -86,7 +86,7 @@ public class PostControllerTest {
 
     @Test
     @WithAnonymousUser // 익명의 회원으로 요청을날림
-    void 포스트수정시_로그인하지않은경우() throws Exception{
+    void 포스트수정시_로그인하지않은경우() throws Exception {
         String title = "title";
         String body = "body";
 
@@ -101,7 +101,7 @@ public class PostControllerTest {
 
     @Test
     @WithMockUser
-    void 포스트수정시_본인이_작성한_글이_아니라면_에러발생() throws Exception{
+    void 포스트수정시_본인이_작성한_글이_아니라면_에러발생() throws Exception {
         String title = "title";
         String body = "body";
 
@@ -110,7 +110,6 @@ public class PostControllerTest {
                 .when(postService)
                 .modify(ArgumentMatchers.eq(title), ArgumentMatchers.eq(body), ArgumentMatchers.any(), ArgumentMatchers.eq(1));
 
-        //로그인 하지 않은 경우
         mockMvc.perform(
                         MockMvcRequestBuilders.put("/api/v1/posts/1")
                                 .contentType(MediaType.APPLICATION_JSON)
@@ -122,7 +121,7 @@ public class PostControllerTest {
 
     @Test
     @WithMockUser
-    void 포스트수정시_수정하려는_글이_없는경우_에러발생() throws Exception{
+    void 포스트수정시_수정하려는_글이_없는경우_에러발생() throws Exception {
         String title = "title";
         String body = "body";
 
@@ -131,7 +130,6 @@ public class PostControllerTest {
                 .when(postService)
                 .modify(ArgumentMatchers.eq(title), ArgumentMatchers.eq(body), ArgumentMatchers.any(), ArgumentMatchers.eq(1));
 
-        //로그인 하지 않은 경우
         mockMvc.perform(
                         MockMvcRequestBuilders.put("/api/v1/posts/1")
                                 .contentType(MediaType.APPLICATION_JSON)
@@ -143,9 +141,8 @@ public class PostControllerTest {
 
     @Test
     @WithMockUser
-    void 포스트삭제() throws Exception{
+    void 포스트삭제() throws Exception {
 
-        //로그인 하지 않은 경우
         mockMvc.perform(
                         MockMvcRequestBuilders.delete("/api/v1/posts/1")
                                 .contentType(MediaType.APPLICATION_JSON)
@@ -155,7 +152,7 @@ public class PostControllerTest {
 
     @Test
     @WithAnonymousUser
-    void 포스트삭제시_로그인하지_않은경우() throws Exception{
+    void 포스트삭제시_로그인하지_않은경우() throws Exception {
 
         //로그인 하지 않은 경우
         mockMvc.perform(
@@ -167,12 +164,11 @@ public class PostControllerTest {
 
     @Test
     @WithMockUser
-    void 포스트삭제시_작성자와_삭제요청자가_다를경우() throws Exception{
+    void 포스트삭제시_작성자와_삭제요청자가_다를경우() throws Exception {
 
         // mocking
         Mockito.doThrow(new SnsApplicationException(ErrorCode.INVALID_PERMISSION)).when(postService).delete(ArgumentMatchers.any(), ArgumentMatchers.any());
 
-        //로그인 하지 않은 경우
         mockMvc.perform(
                         MockMvcRequestBuilders.delete("/api/v1/posts/1")
                                 .contentType(MediaType.APPLICATION_JSON)
@@ -182,16 +178,73 @@ public class PostControllerTest {
 
     @Test
     @WithMockUser
-    void 포스트삭제시_삭제하려는_포스트가_존재하지_않을경우() throws Exception{
+    void 포스트삭제시_삭제하려는_포스트가_존재하지_않을경우() throws Exception {
 
         // mocking
         Mockito.doThrow(new SnsApplicationException(ErrorCode.POST_NOT_FOUND)).when(postService).delete(ArgumentMatchers.any(), ArgumentMatchers.any());
 
-        //로그인 하지 않은 경우
         mockMvc.perform(
                         MockMvcRequestBuilders.delete("/api/v1/posts/1")
                                 .contentType(MediaType.APPLICATION_JSON)
                 ).andDo(MockMvcResultHandlers.print()) // 출력
                 .andExpect(MockMvcResultMatchers.status().isNotFound());
+    }
+
+    @Test
+    @WithMockUser
+    void 피드목록() throws Exception{
+
+        // TODO: mocking
+        Mockito.when(postService.list(ArgumentMatchers.any())).thenReturn(Page.empty());
+
+        mockMvc.perform(
+                        MockMvcRequestBuilders.get("/api/v1/posts")
+                                .contentType(MediaType.APPLICATION_JSON)
+                ).andDo(MockMvcResultHandlers.print()) // 출력
+                .andExpect(MockMvcResultMatchers.status().isOk()); // 정상 동작
+    }
+
+    @Test
+    @WithAnonymousUser
+    void 피드목록요청시_로그인하지_않은경우() throws Exception {
+
+        // TODO: mocking
+        Mockito.when(postService.list(ArgumentMatchers.any())).thenReturn(Page.empty());
+
+        //로그인 하지 않은 경우
+        mockMvc.perform(
+                        MockMvcRequestBuilders.get("/api/v1/posts")
+                                .contentType(MediaType.APPLICATION_JSON)
+                ).andDo(MockMvcResultHandlers.print()) // 출력
+                .andExpect(MockMvcResultMatchers.status().isUnauthorized());
+    }
+
+    @Test
+    @WithMockUser
+    void 내피드목록() throws Exception{
+
+        // TODO: mocking
+        Mockito.when(postService.my(ArgumentMatchers.any(), ArgumentMatchers.any())).thenReturn(Page.empty());
+
+        mockMvc.perform(
+                        MockMvcRequestBuilders.get("/api/v1/posts/my")
+                                .contentType(MediaType.APPLICATION_JSON)
+                ).andDo(MockMvcResultHandlers.print()) // 출력
+                .andExpect(MockMvcResultMatchers.status().isOk()); // 정상 동작
+    }
+
+    @Test
+    @WithAnonymousUser
+    void 내피드목록요청시_로그인하지_않은경우() throws Exception {
+
+        // TODO: mocking
+        Mockito.when(postService.my(ArgumentMatchers.any(), ArgumentMatchers.any())).thenReturn(Page.empty());
+
+        //로그인 하지 않은 경우
+        mockMvc.perform(
+                        MockMvcRequestBuilders.get("/api/v1/posts/my")
+                                .contentType(MediaType.APPLICATION_JSON)
+                ).andDo(MockMvcResultHandlers.print()) // 출력
+                .andExpect(MockMvcResultMatchers.status().isUnauthorized());
     }
 }
