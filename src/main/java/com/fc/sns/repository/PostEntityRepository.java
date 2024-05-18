877229a163e39daa01c037c75ae6fd12bdf6ba54
webdevyoo <yjk7454@naver.com>
2024-05-18 15:16:25 +0900

    Ch.02-13: `PostEntityRepository` - 작성한 포스트 목록 조회 쿼리메소드 추가

--- src/main/java/com/fc/sns/repository/PostEntityRepository.java
+++ src/main/java/com/fc/sns/repository/PostEntityRepository.java
@@ -1,7 +1,13 @@
 package com.fc.sns.repository;
 
+import com.fc.sns.model.Post;
 import com.fc.sns.model.entity.PostEntity;
+import com.fc.sns.model.entity.UserEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PostEntityRepository extends JpaRepository<PostEntity, Integer> {
+
+    Page<PostEntity> findAllByUser(UserEntity userEntity, Pageable pageable);
 }
