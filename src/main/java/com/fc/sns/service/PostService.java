269f8057a6de779482f02021c97d6944bcca1217
webdevyoo <yjk7454@naver.com>
2024-05-21 01:23:58 +0900

    Ch.03-04: `PostService` - 좋아요 및 좋아요 갯수 조회 서비스 레이어 기능 구현

--- src/main/java/com/fc/sns/service/PostService.java
+++ src/main/java/com/fc/sns/service/PostService.java
@@ -3,8 +3,10 @@ package com.fc.sns.service;
 import com.fc.sns.exception.ErrorCode;
 import com.fc.sns.exception.SnsApplicationException;
 import com.fc.sns.model.Post;
+import com.fc.sns.model.entity.LikeEntity;
 import com.fc.sns.model.entity.PostEntity;
 import com.fc.sns.model.entity.UserEntity;
+import com.fc.sns.repository.LikeEntityRepository;
 import com.fc.sns.repository.PostEntityRepository;
 import com.fc.sns.repository.UserEntityRepository;
 import lombok.RequiredArgsConstructor;
@@ -19,6 +21,7 @@ public class PostService {
 
     private final PostEntityRepository postEntityRepository;
     private final UserEntityRepository userEntityRepository;
+    private final LikeEntityRepository likeEntityRepository;
 
     @Transactional
     public void create(String title, String body, String userName) {
@@ -91,5 +94,25 @@ public class PostService {
     @Transactional
     public void like(Integer postId, String userName) {
 
+        UserEntity userEntity = userEntityRepository.findByUserName(userName)
+                .orElseThrow(() -> new SnsApplicationException(ErrorCode.USER_NOT_FOUND, String.format("%s not founded", userName)));
+        // post exist
+        PostEntity postEntity = postEntityRepository.findById(postId)
+                .orElseThrow(() -> new SnsApplicationException(ErrorCode.POST_NOT_FOUND, String.format("%s not founded", postId)));
+
+        // check liked -> throw (이미 좋아요 누른경우 throw)
+        likeEntityRepository.findByUserAndPost(userEntity, postEntity).ifPresent(it -> {
+            throw new SnsApplicationException(ErrorCode.ALREADY_LIKED, String.format("userName %s already like post %d", userName, postId));
+        });
+
+        likeEntityRepository.save(LikeEntity.of(userEntity, postEntity));
+    }
+
+    public Integer likeCount(Integer postId) {
+        // post exist
+        PostEntity postEntity = postEntityRepository.findById(postId)
+                .orElseThrow(() -> new SnsApplicationException(ErrorCode.POST_NOT_FOUND, String.format("%s not founded", postId)));
+
+        return likeEntityRepository.findAllByPost(postEntity).size();
     }
 }
