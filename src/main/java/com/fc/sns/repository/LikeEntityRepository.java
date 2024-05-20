3094d9a0abf8c06b08f37e5d728436a949bd6158
webdevyoo <yjk7454@naver.com>
2024-05-21 01:23:20 +0900

    Ch.03-04: `LikeEntityRepository` - 좋아요 및 좋아요 갯수 조회 쿼리메소드 기능 구현

---
+++ src/main/java/com/fc/sns/repository/LikeEntityRepository.java
@@ -0,0 +1,18 @@
+package com.fc.sns.repository;
+
+import com.fc.sns.model.Post;
+import com.fc.sns.model.entity.LikeEntity;
+import com.fc.sns.model.entity.PostEntity;
+import com.fc.sns.model.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface LikeEntityRepository extends JpaRepository<LikeEntity, Integer> {
+
+    Optional<LikeEntity> findByUserAndPost(UserEntity userEntity, PostEntity postEntity);
+    List<LikeEntity> findAllByPost(PostEntity postEntity);
+
+}
