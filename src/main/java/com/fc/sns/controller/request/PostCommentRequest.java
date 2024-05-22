f82711807e389bd03ad82f5f056d4c36bad2c571
webdevyoo <yjk7454@naver.com>
2024-05-23 00:22:22 +0900

    Ch.03-06: `PostCommentRequest` - 댓글 저장 API용 Request 커맨드 객체용 클래스 추가

--- src/main/java/com/fc/sns/controller/request/PostCommentRequest.java
+++ src/main/java/com/fc/sns/controller/request/PostCommentRequest.java
@@ -2,9 +2,12 @@ package com.fc.sns.controller.request;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
-@Data
+@Getter
 @AllArgsConstructor
+@NoArgsConstructor // 단일필드일 경우 역직렬화시 기본생성자가 필수로 필요하다. https://github.com/FasterXML/jackson-databind/issues/1498
 public class PostCommentRequest {
     private String comment;
 }
