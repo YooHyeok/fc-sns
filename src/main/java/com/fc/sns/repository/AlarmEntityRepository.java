57efd90df5e92b48cc0308430b6bbe389bdfcbb6
webdevyoo <yjk7454@naver.com>
2024-05-25 16:14:28 +0900

    Ch.03-08: `AlarmEntityRepository` - 회원 정보 기준 알람목록 조회 쿼리메소드 구현

---
+++ src/main/java/com/fc/sns/repository/AlarmEntityRepository.java
@@ -0,0 +1,19 @@
+package com.fc.sns.repository;
+
+import com.fc.sns.model.entity.AlarmEntity;
+import com.fc.sns.model.entity.LikeEntity;
+import com.fc.sns.model.entity.PostEntity;
+import com.fc.sns.model.entity.UserEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface AlarmEntityRepository extends JpaRepository<AlarmEntity, Integer> {
+
+    Page<AlarmEntity> findAllByUser(UserEntity userEntity, Pageable pageable);
+
+}
