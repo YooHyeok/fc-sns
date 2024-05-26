3a6c268821d6a641b9dae4b01cd705745fb6892c
webdevyoo <yjk7454@naver.com>
2024-05-26 18:37:02 +0900

    Ch.03-08: `UserController` - 알람 리스트 조회 API 핸들러 매핑 메소드 구현

--- src/main/java/com/fc/sns/controller/UserController.java
+++ src/main/java/com/fc/sns/controller/UserController.java
@@ -2,16 +2,18 @@ package com.fc.sns.controller;
 
 import com.fc.sns.controller.request.UserJoinRequest;
 import com.fc.sns.controller.request.UserLoginRequest;
+import com.fc.sns.controller.response.AlarmResponse;
 import com.fc.sns.controller.response.Response;
 import com.fc.sns.controller.response.UserJoinResponse;
 import com.fc.sns.controller.response.UserLoginResponse;
+import com.fc.sns.model.Alarm;
 import com.fc.sns.model.User;
 import com.fc.sns.service.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/users")
@@ -20,17 +22,20 @@ public class UserController {
 
     private final UserService userService;
 
-    // TODO: implement
     @PostMapping("/join")
     public Response<UserJoinResponse> join(@RequestBody UserJoinRequest request) {
         User user = userService.join(request.getName(), request.getPassword());
         return Response.success(UserJoinResponse.fromUser(user));
     }
 
-    // TODO: implement
     @PostMapping("/login")
     public Response<UserLoginResponse> login(@RequestBody UserLoginRequest request) {
         String token = userService.login(request.getName(), request.getPassword());
         return Response.success(new UserLoginResponse(token));
+    }
+
+    @GetMapping("/alarm")
+    public Response<Page<AlarmResponse>> alarm(Pageable pageable, Authentication authentication) {
+        return Response.success(userService.alarmList(authentication.getName(), pageable).map(AlarmResponse::fromAlarm));
     }
 }
