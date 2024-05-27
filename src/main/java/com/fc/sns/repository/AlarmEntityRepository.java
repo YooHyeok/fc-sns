6d39730c640c1a4ca7d7cde755e7452e5153e846
webdevyoo <yjk7454@naver.com>
2024-05-28 00:54:36 +0900

    Ch.04-02: `AlarmEntityRepository` - UserEntity → UserId 조회 쿼리메소드 리팩토링

--- src/main/java/com/fc/sns/repository/AlarmEntityRepository.java
+++ src/main/java/com/fc/sns/repository/AlarmEntityRepository.java
@@ -6,6 +6,7 @@ import com.fc.sns.model.entity.PostEntity;
 import com.fc.sns.model.entity.UserEntity;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -15,5 +16,7 @@ import java.util.Optional;
 public interface AlarmEntityRepository extends JpaRepository<AlarmEntity, Integer> {
 
     Page<AlarmEntity> findAllByUser(UserEntity userEntity, Pageable pageable);
+
+    Page<AlarmEntity> findAllByUserId(Integer userId, Pageable pageable);
 
 }
