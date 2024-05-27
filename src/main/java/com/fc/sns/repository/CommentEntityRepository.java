5ade4b941852378d200a01683c3825a6593679bc
webdevyoo <yjk7454@naver.com>
2024-05-28 01:03:59 +0900

    Ch.04-02: `CommentEntityRepository` - 게시글 삭제시 연쇄 댓글 삭제 JPQL 구현
    일반적으로 JPA에서 지원하는 쿼리메소드(사용자정의)로 Delete나 Update를 하게되면
    JPA의 기본 원리상 영속성 컨텍스트에 의해 관리되어야 하므로
    해당 데이터를 조회해 와서 영속성 컨텍스트에 저장(스냅샷)한다.
    따라서 일반적인 delete나 update를 하기 위해서는 JPQL을 활용하여 직접적인 Query를 날린다.

--- src/main/java/com/fc/sns/repository/CommentEntityRepository.java
+++ src/main/java/com/fc/sns/repository/CommentEntityRepository.java
@@ -7,7 +7,9 @@ import com.fc.sns.model.entity.UserEntity;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
@@ -15,4 +17,8 @@ import java.util.Optional;
 public interface CommentEntityRepository extends JpaRepository<CommentEntity, Integer> {
 
     Page<CommentEntity> findAllByPost(PostEntity postEntity, Pageable pageable);
+
+    @Modifying
+    @Query(value = "UPDATE LikeEntity entity SET entity.deletedAt = now() WHERE entity.post= :postEntity")
+    void deleteAllByPost(PostEntity postEntity);
 }
