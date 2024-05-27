d564d5075bfda4061b35a1209c6630f9aa5c9068
webdevyoo <yjk7454@naver.com>
2024-05-28 01:03:21 +0900

    Ch.04-02: `LikeEntityRepository` - 게시글 삭제시 연쇄 좋아요 삭제 JPQL 구현
    일반적으로 JPA에서 지원하는 쿼리메소드(사용자정의)로 Delete나 Update를 하게되면
    JPA의 기본 원리상 영속성 컨텍스트에 의해 관리되어야 하므로
    해당 데이터를 조회해 와서 영속성 컨텍스트에 저장(스냅샷)한다.
    따라서 일반적인 delete나 update를 하기 위해서는 JPQL을 활용하여 직접적인 Query를 날린다.

--- src/main/java/com/fc/sns/repository/LikeEntityRepository.java
+++ src/main/java/com/fc/sns/repository/LikeEntityRepository.java
@@ -5,7 +5,9 @@ import com.fc.sns.model.entity.LikeEntity;
 import com.fc.sns.model.entity.PostEntity;
 import com.fc.sns.model.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
@@ -17,4 +19,15 @@ public interface LikeEntityRepository extends JpaRepository<LikeEntity, Integer>
 
     @Query(value = "SELECT COUNT(*) FROM LikeEntity entity WHERE entity.post = :postEntity")
     Integer countByPost(PostEntity postEntity);
+
+
+    /**
+     * 일반적으로 JPA에서 지원하는 쿼리메소드(사용자정의)로 Delete나 Update를 하게되면
+     * JPA의 기본 원리상 영속성 컨텍스트에 의해 관리되어야 하므로
+     * 해당 데이터를 조회해 와서 영속성 컨텍스트에 저장(스냅샷)한다.
+     * 따라서 일반적인 delete나 update를 하기 위해서는 JPQL을 활용하여 직접적인 Query를 날린다.
+     */
+    @Modifying
+    @Query(value = "UPDATE CommentEntity entity SET entity.deletedAt = now() WHERE entity.post= :post")
+    void deleteAllByPost(PostEntity post);
 }
