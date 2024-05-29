d8613aa4dfa57dc4aee3e5e4b7680909c1a49d40
webdevyoo <yjk7454@naver.com>
2024-05-29 13:40:23 +0900

    Ch.04-04: `AuthenticationConfig` - 로그인, 회원가입 permit 현상 WebSecurity에 적용

--- src/main/java/com/fc/sns/configuration/AuthenticationConfig.java
+++ src/main/java/com/fc/sns/configuration/AuthenticationConfig.java
@@ -6,6 +6,7 @@ import com.fc.sns.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -36,15 +37,16 @@ public class AuthenticationConfig extends WebSecurityConfigurerAdapter {
      */
     @Override
     public void configure(WebSecurity web) throws Exception {
-        web.ignoring().regexMatchers("^(?!/api/).*");
+        web.ignoring().regexMatchers("^(?!/api/).*")
+                .antMatchers(HttpMethod.POST, "/api/*/users/join", "/api/*/users/login"); // 회원가입, 로그인은 회원 정보를 가지고 로그인하는 것이므로 어떠한 경우에도 허용
+
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/api/*/users/join", "/api/*/users/login").permitAll() // 회원가입, 로그인은 회원 정보를 가지고 로그인하는 것이므로 어떠한 경우에도 허용
-                .antMatchers("/api/**").authenticated() // 그 외는 모두 인증받아야함
+                .antMatchers("/api/**").authenticated() // 모두 인증받아야함
                 .and()
                 .sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS) // Session을 사용하지 않는 방식 (JWT사용)
