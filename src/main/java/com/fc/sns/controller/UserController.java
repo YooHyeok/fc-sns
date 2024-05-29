7af5072617d1f249b7b254616c454367ab81ef86
webdevyoo <yjk7454@naver.com>
2024-05-30 00:43:06 +0900

    Ch.04-02: `UserController` - 알람 API Principal의 User 객체 동적 타입변환 적용

--- src/main/java/com/fc/sns/controller/UserController.java
+++ src/main/java/com/fc/sns/controller/UserController.java
@@ -6,21 +6,29 @@ import com.fc.sns.controller.response.AlarmResponse;
 import com.fc.sns.controller.response.Response;
 import com.fc.sns.controller.response.UserJoinResponse;
 import com.fc.sns.controller.response.UserLoginResponse;
+import com.fc.sns.exception.ErrorCode;
+import com.fc.sns.exception.SnsApplicationException;
 import com.fc.sns.model.Alarm;
 import com.fc.sns.model.User;
+import com.fc.sns.service.AlarmService;
 import com.fc.sns.service.UserService;
+import com.fc.sns.util.ClassUtils;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/v1/users")
 @RequiredArgsConstructor
 public class UserController {
 
     private final UserService userService;
+    private final AlarmService alarmService;
 
     @PostMapping("/join")
     public Response<UserJoinResponse> join(@RequestBody UserJoinRequest request) {
@@ -37,6 +45,10 @@ public class UserController {
     @GetMapping("/alarm")
     public Response<Page<AlarmResponse>> alarm(Pageable pageable, Authentication authentication) {
 //        return Response.success(userService.alarmList(authentication.getName(), pageable).map(AlarmResponse::fromAlarm));
-        return Response.success(userService.alarmListRefact((User)authentication.getPrincipal(), pageable).map(AlarmResponse::fromAlarm));
+//        return Response.success(userService.alarmListRefact((User)authentication.getPrincipal(), pageable).map(AlarmResponse::fromAlarm));
+        User user = ClassUtils.getSafeCastInstance(authentication.getPrincipal(), User.class)
+                .orElseThrow(() -> new SnsApplicationException(ErrorCode.INTERNAL_SERVER_ERROR, String.format("Casting To User class failed")));
+        return Response.success(userService.alarmListRefact(user, pageable).map(AlarmResponse::fromAlarm));
     }
+
 }
